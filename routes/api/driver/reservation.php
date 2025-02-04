0ceeb3f6382e681036e97d3e8d16c48b5beadf24
afauzansw <afsw.fauzan33142@gmail.com>
2025-02-04 13:12:47 +0700

    feat: base controller, route, and model seeder

---
+++ routes/api/driver/reservation.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\Auth\AuthApiController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(
+    ['prefix' => 'auth', 'as' => 'auth.'],
+    function () {
+        Route::post('login', [AuthApiController::class, 'attempt'])->name('login');
+        Route::post('register', [AuthApiController::class, 'store'])->name('register');
+        Route::post('logout', [AuthApiController::class, 'logout'])->name('logout');
+    }
+);
--- routes/dev.php
+++ routes/dev.php
@@ -4,9 +4,9 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 if (! app()->isProduction()) {
-    Route::get('dev/login/{id}', function ($id = null) {
-        auth()->login(User::find($id));
+    // Route::get('dev/login/{id}', function ($id = null) {
+    //     auth()->login(User::find($id));
 
-        return redirect('/');
-    });
+    //     return redirect('/');
+    // });
 }
